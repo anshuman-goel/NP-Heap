a108cf58d2a8d971a4af8dbbd6379c22af6fb8f6
Anshuman Goel <agoel5@ncsu.edu>
2017-09-02 19:31:36 -0400

    Parameters redifined in remap

--- kernel_module/src/core.c
+++ kernel_module/src/core.c
@@ -53,7 +53,16 @@ int npheap_mmap(struct file *filp, struct vm_area_struct *vma)
 	// Allocating Kernel Memory
 	void* kernel_memory = kmalloc(vma->vm_end - vma->vm_start, GFP_KERNEL);
 	//Creating a mapping from Userspace Virtual Memory to Kernel Logical Memory
-	remap_pfn_range(vma, virt_to_phys((void*)((unsigned long)kernel_memory)), vma->vm_pgoff, ksize(kernel_memory), vma->vm_page_prot);
+	//remap_pfn_range(vma, virt_to_phys((void*)((unsigned long)kernel_memory)), vma->vm_pgoff, ksize(kernel_memory), vma->vm_page_prot);
+	// Ref: https://sites.google.com/site/lbathen/research/mmap_driver
+	if (remap_pfn_range(vma,
+			    vma->vm_start,
+			    virt_to_phys((void *)kernel_memory) >> PAGE_SIZE,
+			    ksize(kernel_memory),
+			    vma->vm_page_prot) < 0) {
+		printk(KERN_ERR "remap_pfn_range failed\n");
+		return -EIO;
+	}
 	printk(KERN_ERR "Size %d\n", ksize(kernel_memory));
     return 0;
 }
