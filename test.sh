86dda9ca8226ffe35f348f8ac31dece039a71277
Anshuman Goel <agoel5@ncsu.edu>
2017-09-14 20:15:27 -0400

    Delete works for first run

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 # sudo insmod kernel_module/npheap.ko
 # sudo chmod 777 /dev/npheap
 rm -f *.log
-./benchmark/benchmark 4 8192 4
+./benchmark/benchmark 256 8192 4
 cat *.log > trace
 sort -n -k3 trace > sorted_trace
-./benchmark/validate 4 8192 < sorted_trace
+./benchmark/validate 256 8192 < sorted_trace
 # rm -f *.log
 # sudo rmmod npheap
